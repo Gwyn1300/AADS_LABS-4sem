2e97d43a9090b20d5ad18e88e27fc8744e8fa84d
Gwyn1300 <maximus010683vfrc@gmail.com>
2026-03-03 19:51:09 +0400

    Added check for element presence and insert element

--- Lab1/set.cpp
+++ Lab1/set.cpp
@@ -1,37 +1,117 @@
 #include <vector>
 
-template <typename T>
-
 class set{
     struct Node{
-    T _data;
+    int _data;
     Node* _left;
     Node* _right;
-    Node(): _data(T(0)), _left(nullptr), _right(nullptr){};
-    Node(T data, Node* left, Node* right): _data(data), _left(left), _right(right){};
-    ~Node() = default;
+    int _hight;
+    Node(): _data(int(0)), _left(nullptr), _right(nullptr), _hight(0){};
+    Node(int data): _data(data), _left(nullptr), _right(nullptr), _hight(1){};
     };
 
-    void clear(Node*root){
-        if (root._left == nullptr&&root._right == nullptr){
-            delete root;
+    Node* root;
+    
+    bool serch(Node*node, int value){
+        if(node->_data == value){
+            return true;
         }
-        clear(root._left);
-        clear(root._right);
+        else if(node->_data >= value&&root->_right!=nullptr){
+            return serch(node->_right, value);
+        }
+        else if(node->_data < value&&root->_left!=nullptr){
+            return serch(node->_left, value);
+        }
+        return false;
+    };
+    
+    int getHeight(Node* node){
+        return (node == nullptr)?-1:node->_hight;
+    };
+    
+    void updateHieght(Node* node){
+        if(node->_left->_hight > node->_right->_hight){
+            node->_hight = node->_left->_hight + 1;
+        }
+        else{
+            node->_hight = node->_right->_hight + 1;
+        }
     };
 
-    Node* _root;
+    int getBalance(Node* node){
+        return (node == nullptr)?0:getHeight(node->_right) - getHeight(node->_left);
+    };
+
+    void swap(Node*a, Node*b){
+        int a_data = a->_data;
+        a->_data = b->_data;
+        b->_data = a_data;
+    }
+
+    void rightRotate(Node* node){
+        swap(node, node->_left);
+        Node* buffer = node->_right;
+        node->_right = node->_left;
+        node->_left = node->_right->_left;
+        node->_right->_left = node->_right->_right;
+        node->_right->_right = buffer;
+        updateHieght(node->_right);
+        updateHieght(node);
+    };
+    
+    void leftRotate(Node* node){
+        swap(node, node->_right);
+        Node* buffer = node->_left;
+        node->_left = node->_right;
+        node->_right = node->_left->_right;
+        node->_left->_right = node->_left->_left;
+        node->_left->_left = buffer;
+        updateHieght(node->_left);
+        updateHieght(node);
+    }
+
+    void balance(Node* node){
+        int balance = getBalance(node);
+        if (balance== -2){
+            if(getBalance(node->_left) == 1){leftRotate(node->_left);}
+            rightRotate(node);
+        }
+        else if(balance == 2){
+            if(getBalance(node->_right) == -1){rightRotate(node->_right);}
+            leftRotate(node);
+        }
+    }
+
+    void insert(Node* node, int value){
+        if(value<node->_data){
+            if(node->_left==nullptr){node->_left = new Node(value);}
+            else insert(node->_left, value);
+        }
+        else if(value>=node->_data){
+            if(node->_right==nullptr){node->_right = new Node(value);}
+            else insert(node->_right, value);
+        }
+        updateHieght(node);
+        balance(node);
+    }
 
     public:
     set():root(nullptr){};
+
     
-    set(std::vector<T>){
-
+    bool contains(int value){
+        return serch(root, value);
     }
 
-    ~set(){clear(_root);}
-
-    set* operator(const set* other){
-
+    bool insert(int value){
+        if(!serch(root, value)){
+            insert(root, value);
+            return true;
+        }
+        return false;
     }
-}
+
+    bool strictly_balance(){
+        
+    }
+};
